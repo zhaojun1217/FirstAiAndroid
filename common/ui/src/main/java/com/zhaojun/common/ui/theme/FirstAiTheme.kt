ce13f8b76d3a0b0a916dda712884e2404d00e08f
zhaojun <zhaojun@mingyangtech.com.cn>
2026-03-31 16:19:40 +0800

    抽离一部分简单资源和主题,统一大项目和两个小module项目的外壳安卓样式

---
+++ common/ui/src/main/java/com/zhaojun/common/ui/theme/FirstAiTheme.kt
@@ -0,0 +1,9 @@
+package com.zhaojun.common.ui.theme
+
+/**
+ *     author : zhaojun
+ *     e-mail : 1334561398@qq.com
+ *     time   : 2026/03/31
+ */
+class Theme {
+}
---
+++ common/ui/src/main/java/com/zhaojun/common/ui/theme/Type.kt
@@ -0,0 +1,9 @@
+package com.zhaojun.common.ui
+
+/**
+ *     author : zhaojun
+ *     e-mail : 1334561398@qq.com
+ *     time   : 2026/03/31
+ */
+class Type {
+}
